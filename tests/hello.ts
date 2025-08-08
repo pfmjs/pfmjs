8b30ad0163f319cf1f65b2eb0a2f2a749f2b50b0
PFM37 <pradhananjana33@gmail.com>
2025-08-08 18:08:10 +0530

    commiting empty repostory to clean up any junk or cached file or folders from github.com

--- tests/hello.ts
+++
@@ -1,9 +0,0 @@
-// built-in fs module
-import * as fs from "../modules/fs.mts";
-
-// ./tests is added because this file will tested from the project root
-// writes file using built-in fs
-fs.writeFile('./tests/helloWorld.txt', 'hi from helloWorld.txt')
-
-const fileContent = fs.readFile('./tests/helloWorld.txt') 
-console.log(fileContent)
